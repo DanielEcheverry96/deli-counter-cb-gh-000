41b60082b1d9341a587df0761a0ed42b1488c713
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-02 06:26:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,9 +19,10 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.empty?
-
+  if !katz_deli.empty?
+    puts "Currently serving #{katz_deli[0]}"
+    katz_deli.shift
+  else
+    puts "There is nobody waiting to be served!"
   end
-  puts "Currently serving #{katz_deli[0]}"
-  katz_deli.shift
 end
