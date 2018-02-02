b3edcb72eaf21a5fe377109966a62e1fd0551494
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-02 06:22:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.empty?
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     mensaje = "The line is currently: "
     katz_deli.each_with_index do |item, index|
